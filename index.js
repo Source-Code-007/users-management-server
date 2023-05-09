f2930ec648263ff8ef7ae1944cec4ff0726681f3
Source-Code-007 <utsho926@gmail.com>
2023-05-09 12:32:29 +0600

    mongodb node version update for mongodb error

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get(('/'), (req, res) => {
 })
 
 
-const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASS}@cluster0.iw4kl2c.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb://utsho926:Wf0nvzmP0dq6OF28@ac-sdycgbe-shard-00-00.iw4kl2c.mongodb.net:27017,ac-sdycgbe-shard-00-01.iw4kl2c.mongodb.net:27017,ac-sdycgbe-shard-00-02.iw4kl2c.mongodb.net:27017/?ssl=true&replicaSet=atlas-12xt4i-shard-0&authSource=admin&retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
